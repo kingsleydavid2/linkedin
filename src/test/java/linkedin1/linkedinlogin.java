8326d2c2b382aa94c6140b6049460749737cba32
Mohan S <kingsleydavid.s@thestreet.com>
2017-06-13 09:37:30 -0700

    Base version

--- src/test/java/linkedin1/linkedinlogin.java
+++ src/test/java/linkedin1/linkedinlogin.java
@@ -1,12 +1,20 @@
 package linkedin1;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
 
 public class linkedinlogin {
+	
+	WebDriver driver;
 	
 	@Test
 	public void linkedin() {
 		
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.navigate().to("http://linkedin.com");
+
 		
 	}
 
